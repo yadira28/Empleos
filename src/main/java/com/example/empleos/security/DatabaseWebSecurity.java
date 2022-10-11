57ca662de7061bab8bc087ccd66cb53df4f12541
yadira28 <yadira.vazquezsantana@cbtis72.edu.mx>
2022-10-10 20:13:37 -0500

    carpeta 13 video 10 personalizar accesos a la url

--- src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
+++ src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
@@ -5,6 +5,7 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
@@ -25,4 +26,25 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 		"where u.username = ?");
 
 	}
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.authorizeRequests()
+		// Los recursos estáticos no requieren autenticación
+		.antMatchers(
+			"/bootstrap/**", 
+			"/images/**",
+			"/tinymce/**",
+			"/logos/**").permitAll()
+		// Las vistas públicas no requieren autenticación
+		.antMatchers("/", 
+			"/signup",
+			"/search",
+			"/vacantes/view/**").permitAll()
+		// Todas las demás URLs de la Aplicación requieren autenticación
+		.anyRequest().authenticated()
+		// El formulario de Login no requiere autenticacion
+		.and().formLogin().permitAll();
+	}
+
+
 }
