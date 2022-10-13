1944fb5d85f4caf17752a9b22ac039e5a2400d21
yadira28 <yadira.vazquezsantana@cbtis72.edu.mx>
2022-10-12 21:52:25 -0500

    carpeta 14 video 10-12 encripte passwords con el algoritmo BCrypt

--- src/main/java/com/example/empleos/controller/HomeController.java
+++ src/main/java/com/example/empleos/controller/HomeController.java
@@ -12,13 +12,16 @@ import org.springframework.data.domain.Example;
 import org.springframework.data.domain.ExampleMatcher;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.example.empleos.model.Perfil;
@@ -39,6 +42,9 @@ public class HomeController {
 	
 	@Autowired
 	private IUsuariosService serviceUsuarios;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
 	
 	@GetMapping("/tabla")
 	public String mostrarTabla(Model model) {
@@ -73,6 +79,11 @@ public class HomeController {
 		
 		return "listado";
 	}
+	@GetMapping("/bcrypt/{texto}")
+	@ResponseBody
+	public String encriptar(@PathVariable("texto") String texto) {
+		return texto + "Encriptado en Bcrypt: " + passwordEncoder.encode(texto);
+	}
 	
 	@GetMapping("/")
 	public String mostrarHome(Model model) {
@@ -102,6 +113,9 @@ public class HomeController {
 	
 	@PostMapping("/signup")
 	public String guardarRegistro(Usuario usuario, RedirectAttributes attributes) {
+		 String pwdPlano = usuario.getPassword();
+		 String pwdEncriptado = passwordEncoder.encode(pwdPlano);
+		 usuario.setPassword(pwdEncriptado);
 		
 		 usuario.setEstatus(1);
 		 usuario.setFechaRegistro(new Date());
--- src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
+++ src/main/java/com/example/empleos/security/DatabaseWebSecurity.java
@@ -3,11 +3,14 @@ package com.example.empleos.security;
 
 import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
@@ -39,6 +42,7 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 		.antMatchers("/", 
 			"/signup",
 			"/search",
+			"/bcrypt/**",
 			"/vacantes/view/**").permitAll()
 		
 		// Asignar permisos a URLs por ROLES
@@ -50,6 +54,13 @@ public class DatabaseWebSecurity extends WebSecurityConfigurerAdapter {
 		// El formulario de Login no requiere autenticacion
 		.and().formLogin().permitAll();
 	}
+	@Bean
+	public PasswordEncoder passwordEncoder() { 
+	return new BCryptPasswordEncoder();
+	}
+
+
+
 
 
 }
